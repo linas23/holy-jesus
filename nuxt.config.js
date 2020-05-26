064df46fb3df7aabde5bf613eb2e75d15d803ceb
linas23 <desemaru77@gmail.com>
2020-05-26 13:20:41 +0545

    .'

--- nuxt.config.js
+++ nuxt.config.js
@@ -94,5 +94,8 @@ export default {
     */
     extend(config, ctx) {
     }
+  },
+  generate: {
+    fallback: true
   }
 }
--- plugins/credential.js
+++ plugins/credential.js
@@ -1,4 +1,4 @@
 export default function ({ $axios }) {
     $axios.setBaseURL('https://api.scripture.api.bible/v1')
-    $axios.setHeader('api-key', process.env.JC_CREDENTIAL)
+    $axios.setHeader('api-key', 'b295d1c8ee8ade4b0197cdf3e924b695')
 }
